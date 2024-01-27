6e49d652b3697d83b076bb916d2786139384fbaf
yaasir cheekoory <yaasir1997@gmail.com>
2024-01-28 02:37:33 +0400

    refactor code

--- src/components/Animation.vue
+++ src/components/Animation.vue
@@ -1,115 +1,26 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+import Scroll from './Scroll.vue'
 
+const langs = ref(["Ruby", "HTML5", "CSS3", "JavaScript", "TypeScript", "SQL", "PHP", "Visual Basic", "C#"]);
+const devTools = ref(["git", "Visual Studio Code", "RubyMine", "Postman", "Chrome", "Sidekick", "Arc", "Edge", "Figma", "Firebase", "Supabase", "Adobe", "Docker", "Heroku"]);
+const frameworks = ref(["Tailwind", "VueJS", "ReactJS", "Nuxt", "Ruby on Rails", "Bootstrap", "Postgresql", "Drupal", "Wordpress", "Umbraco"]);
+const mgmt = ref(["AGILE", "SCRUM", "JIRA", "GitHub", "GitLab", "Trello", "Monday"]);
 </script>
 
 <template>
   <div class="scroll-container">
-    <div class="scroll" style="--time:40s">
-      <div>
-        <span>Ruby</span>
-        <span>HTML5</span>
-        <span>CSS3</span>
-        <span>JavaScript</span>
-        <span>TypeScript</span>
-        <span>SQL</span>
-        <span>PHP</span>
-        <span>Visual Basic</span>
-        <span>SASS</span>
-        <span>C#</span>
-      </div>
-      <div>
-        <span>Ruby</span>
-        <span>HTML5</span>
-        <span>CSS3</span>
-        <span>JavaScript</span>
-        <span>TypeScript</span>
-        <span>SQL</span>
-        <span>PHP</span>
-        <span>Visual Basic</span>
-        <span>SASS</span>
-        <span>C#</span>
-      </div>
-    </div>
-    <div class="scroll-reverse" style="--time:50s">
-      <div>
-        <span>Visual Studio Code</span>
-        <span>RubyMine</span>
-        <span>Postman</span>
-        <span>Chrome</span>
-        <span>Sidekick</span>
-        <span>Arc</span>
-        <span>Edge</span>
-        <span>Figma</span>
-        <span>Firebase</span>
-        <span>Supabase</span>
-      </div>
-      <div>
-        <span>Visual Studio Code</span>
-        <span>RubyMine</span>
-        <span>Postman</span>
-        <span>Chrome</span>
-        <span>Sidekick</span>
-        <span>Arc</span>
-        <span>Edge</span>
-        <span>Figma</span>
-        <span>Firebase</span>
-        <span>Supabase</span>
-      </div>
-    </div>
-    <div class="scroll" style="--time:40s">
-      <div>
-        <span>git</span>
-        <span>Tailwind</span>
-        <span>VueJS</span>
-        <span>ReactJS</span>
-        <span>Nuxt</span>
-        <span>Ruby on Rails</span>
-        <span>Docker</span>
-        <span>Heroku</span>
-        <span>Bootstrap</span>
-        <span>Postgresql</span>
-        <span>Drupal</span>
-        <span>Wordpress</span>
-        <span>Umbraco</span>
-      </div>
-      <div>
-        <span>git</span>
-        <span>Tailwind</span>
-        <span>VueJS</span>
-        <span>ReactJS</span>
-        <span>Nuxt</span>
-        <span>Ruby on Rails</span>
-        <span>Docker</span>
-        <span>Heroku</span>
-        <span>Bootstrap</span>
-        <span>Postgresql</span>
-        <span>Drupal</span>
-        <span>Wordpress</span>
-        <span>Umbraco</span>
-      </div>
-    </div>
-    <div class="scroll-reverse" style="--time:50s">
-      <div>
-        <span>AGILE</span>
-        <span>SCRUM</span>
-        <span>JIRA</span>
-        <span>GitHub</span>
-        <span>GitLab</span>
-        <span>Trello</span>
-        <span>Monday</span>
-      </div>
-      <div>
-        <span>AGILE</span>
-        <span>SCRUM</span>
-        <span>JIRA</span>
-        <span>GitHub</span>
-        <span>GitLab</span>
-        <span>Trello</span>
-        <span>Monday</span>
-      </div>
-    </div>
+    <Scroll timing="--time:40s" cssClass="scroll" :items=langs />
+    <Scroll timing="--time:50s" cssClass="scroll-reverse" :items=devTools />
+    <Scroll timing="--time:40s" cssClass="scroll" :items=frameworks />
+    <Scroll timing="--time:50s" cssClass="scroll-reverse" :items=mgmt />
   </div>
 </template>
 
 <style scoped>
+.scroll-container {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+}
 </style>
---
+++ src/components/Scroll.vue
@@ -0,0 +1,128 @@
+<script setup lang="ts">
+const props = defineProps(['timing', 'cssClass', 'items']);
+const datas = props.items;
+</script>
+
+<template>
+  <div :class=props.cssClass :style=props.timing>
+    <div>
+      <span v-for="data in datas">{{ data }}</span>
+    </div>
+    <div>
+      <span v-for="data in datas">{{ data }}</span>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.scroll {
+  position: relative;
+  display: flex;
+  width: 800px;
+  overflow: hidden;
+  -webkit-mask-image: linear-gradient(90deg, transparent, #fff 20%, #fff 80%, transparent)
+}
+
+.scroll div {
+  white-space: nowrap;
+  animation: scroll var(--time) linear infinite;
+  animation-delay: calc(var(--time)*-1);
+}
+
+.scroll div:nth-child(2) {
+  animation: scroll2 var(--time) linear infinite;
+  animation-delay: calc(var(--time)/-2);
+}
+
+@keyframes scroll {
+  0% {
+    transform: translateX(100%);
+  }
+
+  100% {
+    transform: translateX(-100%);
+  }
+}
+
+@keyframes scroll2 {
+  0% {
+    transform: translateX(0);
+  }
+
+  100% {
+    transform: translateX(-200%);
+  }
+}
+
+.scroll div span {
+  display: inline-flex;
+  margin: 10px;
+  letter-spacing: 0.2em;
+  background: #136F63;
+  color: #fff;
+  padding: 10px 15px;
+  border-radius: 5px;
+  transition: 0.5s;
+}
+
+.scroll div span:hover {
+  background: #2865ba;
+  cursor: pointer;
+}
+
+/* For reverse animation */
+.scroll-reverse {
+  position: relative;
+  display: flex;
+  width: 800px;
+  overflow: hidden;
+  -webkit-mask-image: linear-gradient(90deg, transparent, #fff 20%, #fff 80%, transparent)
+}
+
+.scroll-reverse div {
+  white-space: nowrap;
+  animation: scroll-first-reverse var(--time) linear infinite;
+  animation-delay: calc(var(--time)*-1);
+}
+
+.scroll-reverse div:nth-child(2) {
+  animation: scroll-second-reverse var(--time) linear infinite;
+  animation-delay: calc(var(--time)/-2);
+}
+
+@keyframes scroll-first-reverse {
+  0% {
+    transform: translateX(-100%);
+  }
+
+  100% {
+    transform: translateX(100%);
+  }
+}
+
+@keyframes scroll-second-reverse {
+  0% {
+    transform: translateX(-200%);
+  }
+
+  100% {
+    transform: translateX(0%);
+  }
+}
+
+.scroll-reverse div span {
+  display: inline-flex;
+  margin: 10px;
+  letter-spacing: 0.2em;
+  background: #136F63;
+  color: #fff;
+  padding: 10px 15px;
+  border-radius: 5px;
+  transition: 0.5s;
+}
+
+.scroll-reverse div span:hover {
+  background: #2865ba;
+  cursor: pointer;
+}
+</style>
