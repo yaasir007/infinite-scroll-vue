1b8311bf5d3900a8f3b66e889db43c1626b405fd
yaasir cheekoory <yaasir1997@gmail.com>
2024-01-28 01:19:59 +0400

    it works

---
+++ src/components/Animation.vue
@@ -0,0 +1,115 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="scroll-container">
+    <div class="scroll" style="--time:20s">
+      <div>
+        <span>Ruby</span>
+        <span>HTML5</span>
+        <span>CSS3</span>
+        <span>JavaScript</span>
+        <span>TypeScript</span>
+        <span>SQL</span>
+        <span>PHP</span>
+        <span>Visual Basic</span>
+        <span>SASS</span>
+        <span>C#</span>
+      </div>
+      <div>
+        <span>Ruby</span>
+        <span>HTML5</span>
+        <span>CSS3</span>
+        <span>JavaScript</span>
+        <span>TypeScript</span>
+        <span>SQL</span>
+        <span>PHP</span>
+        <span>Visual Basic</span>
+        <span>SASS</span>
+        <span>C#</span>
+      </div>
+    </div>
+    <div class="scroll" style="--time:40s">
+      <div>
+        <span>Visual Studio Code</span>
+        <span>RubyMine</span>
+        <span>Postman</span>
+        <span>Chrome</span>
+        <span>Sidekick</span>
+        <span>Arc</span>
+        <span>Edge</span>
+        <span>Figma</span>
+        <span>Firebase</span>
+        <span>Supabase</span>
+      </div>
+      <div>
+        <span>Visual Studio Code</span>
+        <span>RubyMine</span>
+        <span>Postman</span>
+        <span>Chrome</span>
+        <span>Sidekick</span>
+        <span>Arc</span>
+        <span>Edge</span>
+        <span>Figma</span>
+        <span>Firebase</span>
+        <span>Supabase</span>
+      </div>
+    </div>
+    <div class="scroll" style="--time:40s">
+      <div>
+        <span>git</span>
+        <span>Tailwind</span>
+        <span>VueJS</span>
+        <span>ReactJS</span>
+        <span>Nuxt</span>
+        <span>Ruby on Rails</span>
+        <span>Docker</span>
+        <span>Heroku</span>
+        <span>Bootstrap</span>
+        <span>Postgresql</span>
+        <span>Drupal</span>
+        <span>Wordpress</span>
+        <span>Umbraco</span>
+      </div>
+      <div>
+        <span>git</span>
+        <span>Tailwind</span>
+        <span>VueJS</span>
+        <span>ReactJS</span>
+        <span>Nuxt</span>
+        <span>Ruby on Rails</span>
+        <span>Docker</span>
+        <span>Heroku</span>
+        <span>Bootstrap</span>
+        <span>Postgresql</span>
+        <span>Drupal</span>
+        <span>Wordpress</span>
+        <span>Umbraco</span>
+      </div>
+    </div>
+    <div class="scroll" style="--time:20s">
+      <div>
+        <span>AGILE</span>
+        <span>SCRUM</span>
+        <span>JIRA</span>
+        <span>GitHub</span>
+        <span>GitLab</span>
+        <span>Trello</span>
+        <span>Monday</span>
+      </div>
+      <div>
+        <span>AGILE</span>
+        <span>SCRUM</span>
+        <span>JIRA</span>
+        <span>GitHub</span>
+        <span>GitLab</span>
+        <span>Trello</span>
+        <span>Monday</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+</style>
